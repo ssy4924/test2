f8316a2cdc95cc78ea64bf879264f2d2aa0a835b
kb <kb@davids>
2022-04-04 21:23:41 +0900

    배포하기위한 소스수정

--- test2/src/main/java/kr/co/kblife/Test2Application.java
+++ test2/src/main/java/kr/co/kblife/Test2Application.java
@@ -2,12 +2,23 @@ package kr.co.kblife;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Test2Application {
+public class Test2Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test2Application.class, args);
 	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) { 
+		return builder.sources(Test2Application.class); 
+		
+}
+	
+
+	
+	
 
 }
